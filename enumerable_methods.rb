21023c87ba1032c131fb3e8fef87cf03ec564644
petumazo <davidalvarezmazzo@gmail.com>
2021-04-22 17:34:44 -0500

    Add extra case scenario for doble check

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,26 +1,36 @@
-
+#1.my_each
 def my_each(array)
     index = 0
     while index <= (array.length - 1)
-        yield(array[index])
+        yield (array[index])
         index += 1
     end
 end
-
 # case scenario given
 ar1 = [ "a", "b", "c" ]
-puts "this is my_each #{my_each(ar1) {|x| print x, " -- " }}"
+puts "This is my_each #{my_each(ar1) {|x| print x, " -- " }}"
+puts " This is my_each #{my_each(ar1) {|x| print x*2}}"
 
 
-
+#2.my_each_with_index
 def my_each_with_index(array)
     index = 0
     while index <= (array.length - 1)
-        print index 
+        yield index
         index += 1
     end
 end
-
 # case scenario given
 ar2 = [ "a", "b", "c" ]
-puts "This is my_each_with_index #{my_each_with_index(ar2) {|x| print x, " -- " }}"
+puts "This is my_each_with_index #{my_each(ar1) {|x| print x, " -- " }}"
+puts " This is my_each_with_index  #{my_each_with_index(ar2) {|x| print x*2}}"
+
+
+# 3.my_select
+def my_select(array)
+    
+    
+end
+
+a = %w[ a b c d e f ]
+my_select(a) {|v| v =~ /[aeiou]/ }    #=> ["a", "e"]
