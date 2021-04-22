a7b5e70123788f6c8336a1f8381d54f65d8eb2ab
petumazo <davidalvarezmazzo@gmail.com>
2021-04-22 17:56:36 -0500

    Add my select method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -28,9 +28,14 @@ puts " This is my_each_with_index  #{my_each_with_index(ar2) {|x| print x*2}}"
 
 # 3.my_select
 def my_select(array)
-    
-    
+    index = 0
+    arr = Array[]
+    while index <= (array.length - 1)
+            arr << array[index] if yield array[index]
+        index += 1
+    end
+    arr
 end
-
+# case scenario given
 a = %w[ a b c d e f ]
-my_select(a) {|v| v =~ /[aeiou]/ }    #=> ["a", "e"]
+print my_select(a) {|v| v =~ /[aeiou]/ }    #=> ["a", "e"]
