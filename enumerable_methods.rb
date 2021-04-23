4e971bb159f394a9be656c47dd37c30c62e7d0e2
petumazo <davidalvarezmazzo@gmail.com>
2021-04-23 12:14:47 -0500

    Delete commented code

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,58 +1,3 @@
-# #1.my_each
-# def my_each(array)
-#     index = 0
-#     while index <= (array.length - 1)
-#         yield (array[index])
-#         index += 1
-#     end
-# end
-# # case scenario given
-# ar1 = [ "a", "b", "c" ]
-# puts "This is my_each #{my_each(ar1) {|x| print x, " -- " }}"
-# puts " This is my_each #{my_each(ar1) {|x| print x*2}}"
-
-
-# #2.my_each_with_index
-# def my_each_with_index(array)
-#     index = 0
-#     while index <= (array.length - 1)
-#         yield index
-#         index += 1
-#     end
-# end
-# # case scenario given
-# ar2 = [ "a", "b", "c" ]
-# puts "This is my_each_with_index #{my_each(ar1) {|x| print x, " -- " }}"
-# puts " This is my_each_with_index  #{my_each_with_index(ar2) {|x| print x*2}}"
-
-
-# # 3.my_select
-# def my_select(array)
-#     index = 0
-#     arr = Array[]
-#     while index <= (array.length - 1)
-#             arr << array[index] if yield array[index]
-#         index += 1
-#     end
-#     arr
-# end
-# # case scenario given
-# a = %w[ a b c d e f ]
-# print my_select(a) {|v| v =~ /[aeiou]/ }    #=> ["a", "e"]
-
-# #my all
-# def my_all?
-#     result = true
-#     my_each do |item|
-#         result = false if !yeild(item)
-#     end
-#     result
-# end
-
-# # case scenario given
-# a = %w[ a b c d e f ]
-# print my_all(a) {|v| v =~ /[aeiou]/ }    #=> ["a", "e"]
-
 module Enumerable
     #1. my_each method
     def my_each
@@ -70,7 +15,7 @@ module Enumerable
     def my_select
         result = []
         my_each do |arr_item|
-          result << arr_item if yield(arr_item)
+            result << arr_item if yield(arr_item)
         end
         result
     end
